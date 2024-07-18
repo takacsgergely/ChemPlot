bc700cb878c8a7b9b22a69f72a05b3436264c018
takacsg <gergely.takacs@mcule.com>
2024-07-18 14:17:09 +0200

    Enhanced functionality with custom palettes and modified dot sizes

--- chemplot/utils.py
+++ chemplot/utils.py
@@ -1,18 +1,16 @@
 # Authors: Murat Cihan Sorkun <mcsorkun@gmail.com>, Dajt Mullaj <dajt.mullai@gmail.com>
 #
-# License: BSD 3 clause
+# License: BSD 3 clause 
+import pkg_resources
+import pandas as pd
 import re
 
-import pandas as pd
-import pkg_resources
-
-from chemplot.parameters import INFO_DATASET, SAMPLE_DATASETS
-
+from chemplot.parameters import SAMPLE_DATASETS, INFO_DATASET
 
 def load_data(name):
     """
     Returns one of the sample datasets.
-
+    
     :param name: Name of the sample dataset
     :type name: string
     :returns: The Dataframe of the sample dataset
@@ -21,14 +19,13 @@ def load_data(name):
 
     name = _select_dataset(name)
 
-    stream = pkg_resources.resource_stream(__name__, f"data/{name}.csv")
+    stream = pkg_resources.resource_stream(__name__, f'data/{name}.csv')
     return pd.read_csv(stream)
-
 
 def _select_dataset(name):
     """
     Returns one of the sample datasets.
-
+    
     :param name: A version of the name of the sample dataset
     :type name: string
     :returns: The name of the sample dataset file
@@ -40,7 +37,6 @@ def _select_dataset(name):
             return key
 
     raise Exception(f'"{name}" cannot be found in the sample datasets')
-
 
 def info_data():
     """
